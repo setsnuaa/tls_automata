976c381b2d524d3b1897356b8c154ece9bfffb79
setsnuaa <1051338166@qq.com>
2022-12-05 22:11:29 +0800

    update

--- src/infer_client.py
+++ src/infer_client.py
@@ -12,5 +12,9 @@ from pylstar.Letter import Letter
 from pylstar.Word import Word
 
 import config.scenarios
+import util.tls_args
+from util.utils import fill_answer_with,get_expected_output,read_next_msg
+from HappyPathFirst import HappyPathFirst
+
 
 
