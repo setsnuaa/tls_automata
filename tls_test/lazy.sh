61d5125917a274cdf770e3de1a757ff76ab471c1
setsnuaa <1051338166@qq.com>
2023-02-08 20:44:08 +0800

    init

--- tls_test/lazy.sh
+++ tls_test/lazy.sh
@@ -5,5 +5,23 @@
 # openssl
 for file in stacks/openssl-git--1_1--3_0/tags/*
 do
-  echo $file
+  make containers/openssl/"$(basename $file)".docker
+done
+for file in stacks/openssl-git--1_0/tags/*
+do
+  make containers/openssl/"$(basename $file)".docker
+done
+for file in stacks/openssl-git--0_9_8/tags/*
+do
+  make containers/openssl/"$(basename $file)".docker
+done
+# gnutls
+for file in stacks/gnutls-git/tags/*
+do
+  make containers/gnutls/"$(basename $file)".docker
+done
+# boringssl
+for file in stacks/boringssl-git/tags/*
+do
+  make containers/boringssl/"$(basename $file)".docker
 done
