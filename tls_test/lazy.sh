781274acb561f9428d17f0bdb7e40dca711c8644
setsnuaa <1051338166@qq.com>
2023-02-08 18:13:06 +0800

    init

--- tls_test/lazy.sh
+++ tls_test/lazy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-make crypto-material
-# tool
-make containers/tool/tls-inferer.docker
+#make crypto-material
+## tool
+#make containers/tool/tls-inferer.docker
 # openssl
 for file in stacks/openssl-git--1_1--3_0
 do
-  echo 'containers/openssl/$file.docker'
+  echo $file
 done
