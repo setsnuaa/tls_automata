bbdeeb153b0a2ca3ed329c2f334b9f0689bf48a7
setsnuaa <1051338166@qq.com>
2023-02-09 21:57:06 +0800

    update

--- tls_test/lazy.sh
+++ tls_test/lazy.sh
@@ -26,7 +26,11 @@ done
 #  make containers/boringssl/"$(basename $file)".docker
 #done
 # wolfssl
-for file in stacks/wolfssl-git/tags/*
+for file in stacks/wolfssl-git--2_0_0/tags/*
+do
+  make containers/wolfssl/"$(basename $file)".docker
+done
+for file in stacks/wolfssl-git--3_15_5/tags/*
 do
   make containers/wolfssl/"$(basename $file)".docker
 done
