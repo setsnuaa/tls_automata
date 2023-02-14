8510321fc31e77f39a8290bda8e2301ede583d86
setsnuaa <1051338166@qq.com>
2023-02-14 15:39:50 +0800

    update

--- tls_test/mbedtls.sh
+++ tls_test/mbedtls.sh
@@ -43,11 +43,66 @@ done
 docker rmi `docker images | grep mbedtls | awk '{print $3}'`
 docker image prune -f
 
-for file in stacks/mbedtls-git4/tags/*
+for file in stacks/mbedtls-git/tags4/*
 do
   make containers/mbedtls/"$(basename $file)".docker
 done
-for file in stacks/mbedtls-git4/tags/*
+for file in stacks/mbedtls-git/tags4/*
+do
+  ./infer-client.sh tls-test/mbedtls:"$(basename $file)" 1.2 tls12
+done
+docker rmi `docker images | grep mbedtls | awk '{print $3}'`
+docker image prune -f
+
+for file in stacks/mbedtls-git/tags5/*
+do
+  make containers/mbedtls/"$(basename $file)".docker
+done
+for file in stacks/mbedtls-git/tags5/*
+do
+  ./infer-client.sh tls-test/mbedtls:"$(basename $file)" 1.2 tls12
+done
+docker rmi `docker images | grep mbedtls | awk '{print $3}'`
+docker image prune -f
+
+for file in stacks/mbedtls-git/tags6/*
+do
+  make containers/mbedtls/"$(basename $file)".docker
+done
+for file in stacks/mbedtls-git/tags6/*
+do
+  ./infer-client.sh tls-test/mbedtls:"$(basename $file)" 1.2 tls12
+done
+docker rmi `docker images | grep mbedtls | awk '{print $3}'`
+docker image prune -f
+
+for file in stacks/mbedtls-git/tags7/*
+do
+  make containers/mbedtls/"$(basename $file)".docker
+done
+for file in stacks/mbedtls-git/tags7/*
+do
+  ./infer-client.sh tls-test/mbedtls:"$(basename $file)" 1.2 tls12
+done
+docker rmi `docker images | grep mbedtls | awk '{print $3}'`
+docker image prune -f
+
+for file in stacks/mbedtls-git/tags8/*
+do
+  make containers/mbedtls/"$(basename $file)".docker
+done
+for file in stacks/mbedtls-git/tags8/*
+do
+  ./infer-client.sh tls-test/mbedtls:"$(basename $file)" 1.2 tls12
+done
+docker rmi `docker images | grep mbedtls | awk '{print $3}'`
+docker image prune -f
+
+for file in stacks/mbedtls-git/tags9/*
+do
+  make containers/mbedtls/"$(basename $file)".docker
+done
+for file in stacks/mbedtls-git/tags9/*
 do
   ./infer-client.sh tls-test/mbedtls:"$(basename $file)" 1.2 tls12
 done
