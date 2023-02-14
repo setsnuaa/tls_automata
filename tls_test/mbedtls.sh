a0bc4de261450533108077b4ba7437095f382c73
setsnuaa <1051338166@qq.com>
2023-02-14 15:26:47 +0800

    update

--- tls_test/mbedtls.sh
+++ tls_test/mbedtls.sh
@@ -3,3 +3,48 @@ for file in stacks/mbedtls-git/tags/*
 do
   make containers/mbedtls/"$(basename $file)".docker
 done
+for file in stacks/mbedtls-git/tags/*
+do
+  ./infer-client.sh tls-test/mbedtls:"$(basename $file)" 1.2 tls12
+done
+docker rmi `docker images | grep mbedtls | awk '{print $3}'`
+
+for file in stacks/mbedtls-git/tags1/*
+do
+  make containers/mbedtls/"$(basename $file)".docker
+done
+for file in stacks/mbedtls-git/tags1/*
+do
+  ./infer-client.sh tls-test/mbedtls:"$(basename $file)" 1.2 tls12
+done
+docker rmi `docker images | grep mbedtls | awk '{print $3}'`
+
+for file in stacks/mbedtls-git/tags2/*
+do
+  make containers/mbedtls/"$(basename $file)".docker
+done
+for file in stacks/mbedtls-git/tags2/*
+do
+  ./infer-client.sh tls-test/mbedtls:"$(basename $file)" 1.2 tls12
+done
+docker rmi `docker images | grep mbedtls | awk '{print $3}'`
+
+for file in stacks/mbedtls-git/tags3/*
+do
+  make containers/mbedtls/"$(basename $file)".docker
+done
+for file in stacks/mbedtls-git/tags3/*
+do
+  ./infer-client.sh tls-test/mbedtls:"$(basename $file)" 1.2 tls12
+done
+docker rmi `docker images | grep mbedtls | awk '{print $3}'`
+
+for file in stacks/mbedtls-git4/tags/*
+do
+  make containers/mbedtls/"$(basename $file)".docker
+done
+for file in stacks/mbedtls-git4/tags/*
+do
+  ./infer-client.sh tls-test/mbedtls:"$(basename $file)" 1.2 tls12
+done
+docker rmi `docker images | grep mbedtls | awk '{print $3}'`
